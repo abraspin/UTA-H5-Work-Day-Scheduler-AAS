b8437a34138688b268e83fa00ffb0dbf2ae02af9
abraspin <abrahamspindel@gmail.com>
2020-08-20 15:45:33 -0500

    add prefill function and save click functionality

--- javascript.js
+++ javascript.js
@@ -1,89 +1,59 @@
 // First thing we do is wrap everything up in a nice document.ready call
 $(document).ready(function () {
   $("#currentDay").html(moment().format("dddd, MMMM Do YYYY"));
+
+  //call function to render the row elements
   renderRows();
 
-  //   var testHour = moment("10:00am", "h:mma");
-  //   console.log("testHour", testHour.format("HH"));
-
-  //   if (moment(testHour).isBefore(moment())) {
-  //     console.log("it's before the time you put in!");
-  //   } else if (moment(testHour).isAfter(moment())) {
-  //     console.log("it's after the time you put in!");
-  //   }
-
-  //   var beginningTime = moment();
-  //     console.log("isBefore -> beginningTime", beginningTime);
-  //   var beginningTime = moment().format().getHours;
-  //   console.log("isBefore -> beginningTime", beginningTime);
-  console.log(moment());
+  // call the function to prefill todo fields with pre-saved to-do strings
+  prefill();
 });
-//TODO: how am I linked to moment? did i do that?
-
-function parseTime() {}
-
-//returns true if the time passed in is before the current time
-function isBefore(time) {
-  //   var beginningTime = moment();
-  //   console.log("isBefore -> beginningTime", beginningTime);
-}
+//how am I linked to moment? did i do that? is it part of javscript?
 
 ////////////////////////////////////////RENDER ROWS//////////////////////////////////////////
 function renderRows() {
   var mainBodyEl = $("#main-body");
+  mainBodyEl.addClass("time-block");
 
   // loop through and add an hour for each time
   for (var i = 9; i < 18; i++) {
     // Building block for each hour element,
     // starting with a div with ID classes for hour names (can't figure out how to dynamically add id oh well)
-    var newRow = $("<div class=' row time-block col-md-12 border text-right'>  </div>");
-    var dayHour;
-    // if it's am, append "am"
-    // if (i < 12) {
-    //   //   newRow.addClass("hour-" + i + "-am ");
-    //   dayHour = moment(i + ":00am", "h:mma");
+    var newRow = $("<div class='row col-md-12 '>  </div>");
+    newRow.addClass("row-wrapper-" + i);
 
-    //   // add pm if it's 12 noon
-    // } else if (i === 12) {
-    //   //   newRow.addClass("hour-" + i + "-pm ");
-    //   dayHour = moment("12:00pm", "h:mma");
+    // this variable holds the time of day each hour row represents
+    var dayHour = moment(i, "h");
 
-    //   // subtract 12 to get 12-hour pm time, and append pm
-    // } else if (i > 12) {
-    //   //   newRow.addClass("hour-" + (i - 12) + "-pm ");
-    //   var temp = i - 12;
-    //   dayHour = moment(temp + ":00", "hh:A");
-    //   console.log(dayHour);
-    // }
-
-    dayHour = moment(i, "h");
-
-    // TODO: How the heck am I gonna do present? probably I need to do a comparison with hours only? or math.floor?
-    //  how do I use moment.js to compare ONLY the hour?
-
-    //then we see if this hour row is before or after the current time, and assign its class accordingly (to be hooked with CSS)
-    if (dayHour.isBefore(moment())) {
-      newRow.addClass("past");
-    }
-    if (dayHour.isAfter(moment())) {
-      newRow.addClass("future");
-    }
-    if (dayHour.isSame(moment(), "hour")) {
-      newRow.addClass("present");
-    }
     // Then we add the 1st column, which contains the hour
     // TODO: experiment with a template literal here!!
-    var hourColumn = $("<div class='hour col-md-3 border'></div>");
-    //the format method returns I think just a string
+    var hourColumn = $("<div class='text-right hour col-md-3 border'></div>");
     // this one line of code took like 20 minutes...one line...LEARNING!
     hourColumn.text(dayHour.format("ha"));
-
     newRow.append(hourColumn);
-    // Next  we add the column which contains the todo item form
-    // var toDoColumn = $("<input type='text' id='todo-input' class = 'col-md-4' placeholder='' name='todo-text'/>");
 
+    // Next  we add the column which contains the todo item form
+    // TODO: how do I make it so that I can hit "enter"? do I have to...somehow tie an event listener to the button?
+    // TODO: or have I gone the totally wrong way about setting up the relationship between button and form?
+    // TODO: or is it just one more event listener that is independent of the button?
+    // also get rid of that test ID
     var toDoColumn = $("<input type='text' class='col-md-6' id='test' placeholder=''></input>");
+
+    //we need this tag to match it with its corresponding save button
     toDoColumn.addClass("row-" + i);
+
+    //then we see if this hour row is before or after the current time, and assign THE FORM ELEMENT its
+    //  class accordingly (to be hooked with CSS)
+    if (dayHour.isBefore(moment())) {
+      toDoColumn.addClass("past");
+    }
+    if (dayHour.isAfter(moment())) {
+      toDoColumn.addClass("future");
+    }
+    if (dayHour.isSame(moment(), "hour")) {
+      toDoColumn.addClass("present");
+    }
+
     newRow.append(toDoColumn);
     //might have to do class and pass that pesky i
 
@@ -99,12 +69,32 @@ function renderRows() {
     // add our beautiful new dynamically rendered hour row
     mainBodyEl.append(newRow);
   }
-
-  $(".saveBtn").on("click", function (event) {
-    event.preventDefault();
-    var hourRowToSave = event.currentTarget.classList[2];
-    var hourRowToSaveEl = $("." + hourRowToSave + "");
-    var toDoItem = hourRowToSaveEl.val();
-    localStorage.setItem(hourRowToSave, toDoItem);
-  });
 }
+//TODO:
+//FIXME://TODO:
+//FIXME://TODO:
+//FIXME:
+
+////////////////////////PRE FILL PAGE FROM LOCAL STORAGE FUNCTION//////////////////
+function prefill() {
+  for (var i = 9; i < 18; i++) {
+    // each hour's todo item is stored in local storage with key format "row-#"
+    var storedToDo = localStorage.getItem("row-" + i) || "";
+    console.log("prefill -> storedToDo", storedToDo);
+
+    // fill the value of the form element with its corresponding todo item
+    // $(".row-" + i).text(storedToDo);
+    // $(".row-" + i).value = storedToDo;
+    $(".row-" + i).child.text("test!");
+  }
+}
+
+/////////////////////////EVENT LISTENERS/////////////////////////
+$(".saveBtn").on("click", function (event) {
+  //TODO:   is this prevent default necessary?
+  event.preventDefault();
+  var hourRowToSave = event.currentTarget.classList[2];
+  var hourRowToSaveEl = $("." + hourRowToSave + "");
+  var toDoItem = hourRowToSaveEl.val();
+  localStorage.setItem(hourRowToSave, toDoItem);
+});
