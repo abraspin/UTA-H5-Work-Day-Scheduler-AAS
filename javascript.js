072ccd674dad3fe61d2f81743f1a8811043b648b
abraspin <abrahamspindel@gmail.com>
2020-08-22 19:45:31 -0500

    clean code, remove dev logs, style timeout spinner element

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-//TODO: README embedded picture size control?
+//this displays a spinner in the main block in case a CDN call jams up or something
+displayLoadingSpinner();
 
 // First thing we do is wrap everything up in a nice document.ready call
-$(document).ready(function () {
+$(document).ready(function (event) {
   //set the top box to display the current date
   $("#currentDay").html(moment().format("dddd, MMMM Do YYYY"));
 
@@ -14,53 +15,49 @@ $(document).ready(function () {
   // this function adds the clear all button
   renderClearAllButton($("#main-body"));
 
-  // TODO: apparently event listeners need to be inside of the document.ready??? I am so sure it was working before with
-  // it all the way at the end and I'm absolutely bewildered why suddenly it stopped saving until i moved it here...
+  // Now that the page is nice and rendered we can remove our lovely spinner
+  $(".fa-spinner").remove();
+
   /////////////////////////EVENT LISTENERS/////////////////////////
-  /////////////////////////////////////////////////////////////////
 
   ///////////////////////INDIVIDUAL SAVE BUTTONS /////////////////
   $(".saveBtn").on("click", function (event) {
-    //TODO:   is this prevent default necessary?
     event.preventDefault();
-    // console.log("I got clicked!");
 
     // hourRowToSave will have format "row-#"
     var hourRowToSave = event.currentTarget.classList[2];
-    // console.log("hourRowToSave", hourRowToSave);
 
     // this is a reference to the entire row element containing hour, input, and save
     var hourRowToSaveEl = $("." + hourRowToSave + "");
 
     // this variable contains the todo string the user just saved
     var toDoItem = hourRowToSaveEl.val();
-    // console.log("toDoItem", toDoItem);
 
     // save the todo item to local storage with key "row-#"
     localStorage.setItem(hourRowToSave, toDoItem);
+
+    // alternative way to capture adjacent element!
+    // var anyVariableName = $(this).siblings("input").val();
   });
+
+  // Alternative click listener: ANY button with saveBtn class in main body el
+  // $("#main-body").on("click", ".saveBtn", function(){
+  // })
 
   /////////////////// CLEAR SINGLE TODO ////////////////////
-  $(".clear-hour").on("click", function (event) {
-    // event.preventDefault();
-    // console.log("im the clear hour function");
-    // hourRowToClear will have format "row-#"
-
-    //This is an awful magic number that refers to the class name of the row we're currently referring to
-    var hourRowToClear = event.currentTarget.classList[6];
-    // console.log("hourRowToClear", hourRowToClear);
-
-    // // this is a reference to the entire row element containing hour, input, and save
-    var hourRowToClearEl = $("." + hourRowToClear + "");
-
-    // // this variable contains the todo string the user just saved
-    // // var toDoItem = hourRowToSaveEl.val();
-    hourRowToClearEl.val("");
-    // console.log("toDoItem", toDoItem);
-
-    // // save the todo item to local storage with key "row-#"
-    localStorage.setItem(hourRowToClear, "");
-  });
+  /// the styling for clear row button fails at mobile screen size, so click event is deactivated.
+  // $(".clear-hour").on("click", function (event) {
+  //   event.preventDefault();
+  //   // hourRowToClear will have format "row-#"
+  //   //This contains a magic number that refers to the class name of the row we're currently referring to
+  //   var hourRowToClear = event.currentTarget.classList[6];
+  //   // this is a reference to the entire row element containing hour, input, and save btn
+  //   var hourRowToClearEl = $(`.${hourRowToClear}`);
+  //   // Empty out the form field on the page
+  //   hourRowToClearEl.val("");
+  //   // save blank todo string to local storage with key "row-#"
+  //   localStorage.setItem(hourRowToClear, "");
+  // });
 
   ///////////////////////CLEAR ALL TODOS  /////////////////
   $(".clear-hours-all").on("click", function (event) {
@@ -86,23 +83,17 @@ function renderRows() {
     var dayHour = moment(i, "h");
 
     // Then we add the 1st column, which contains the hour
-    // TODO: experiment with a template literal here!!
-    var hourColumn = $("<div class='text-right hour col-md-3'></div>");
+    var hourColumn = $("<div class='text-right hour col-md-3 '></div>");
 
     //format and append the hour for this row
     hourColumn.text(dayHour.format("ha"));
     newRow.append(hourColumn);
 
     // Next  we add the column which contains the todo item form
-    // TODO: how do I make it so that I can hit "enter"? do I have to...somehow tie an event listener to the button?
-    // TODO: or have I gone the totally wrong way about setting up the relationship between button and form?
-    // TODO: or is it just one more event listener that is independent of the button?
-    // also get rid of that test ID
-    //TODO: this is the col-md class I modify to fit the delete button
-    var toDoColumn = $("<input type='text' class='col-md-7' id='test' placeholder=''></input>");
+    var toDoColumn = $("<input type='text' class='col-md-7 ' id='test' placeholder=''></input>");
 
     //we need this tag to match it with its corresponding save button
-    toDoColumn.addClass("row-" + i);
+    toDoColumn.addClass(`row-${i}`);
 
     // then we see if this hour row is before or after the current time, and assign THE FORM ELEMENT its
     // class accordingly (to be hooked with CSS) for time-based coloring
@@ -117,7 +108,6 @@ function renderRows() {
     }
 
     newRow.append(toDoColumn);
-    //might have to do class and pass that pesky i
 
     // finally we add the third column, a save button
     var saveButtonColumn = $("<button  class = 'saveBtn col-md-2' />");
@@ -131,7 +121,7 @@ function renderRows() {
     //append the button in all its splendor to the new row
     newRow.append(saveButtonColumn);
 
-    //TODO:
+    // Bug: this button functions but will not style correctly on mobile, so it is deactivated.
     // Render the button on the end of each row to clear that row only
     // renderClearRowButton(newRow, i);
 
@@ -145,7 +135,6 @@ function prefill() {
   for (var i = 9; i < 18; i++) {
     // each hour's todo item is stored in local storage with key format "row-#"
     var storedToDo = localStorage.getItem("row-" + i) || "";
-    console.log("prefill -> storedToDo", storedToDo);
 
     $(".row-" + i).val(storedToDo);
   }
@@ -154,8 +143,8 @@ function prefill() {
 ////////////////////////RENDER THE BOTTOM 'CLEAR ALL' BUTTON//////////////////
 function renderClearAllButton(bodyElement) {
   var clearToDoButton = $("<button  class = 'clear-hours-all my-2 btn btn-primary  center col-md-3' />");
-  //TODO: how do I do a newline? /n? /r? /n/r/ ?? ?
   clearToDoButton.text("Click Here to clear all To-do's");
+
   bodyElement.append(clearToDoButton);
 }
 
@@ -169,11 +158,19 @@ function clearAllToDos() {
 }
 
 ////////////////////////RENDER INDIVIDUAL ROW-CLEAR BUTTONS //////////////////
-function renderClearRowButton(rowElement, i) {
-  var newDiv = $("<div class='text-right'><div>");
-  var clearToDoButton = $("<button  class = 'clear-hour ml-5 text-right btn btn-danger  center ' />");
-  clearToDoButton.addClass("row-" + i);
-  newDiv.append(clearToDoButton);
-  clearToDoButton.append($("<i class='fa fa-trash fa-lg'></i>"));
-  rowElement.append(newDiv);
+/// the styling for the clear row button fails at mobile screen size, so function is deactivated.
+// function renderClearRowButton(rowElement, i) {
+//   var newDiv = $("<div class='text-right'><div>");
+//   var clearToDoButton = $("<button  class = 'clear-hour ml-5 text-right btn btn-danger  center col-sm-3 ' />");
+//   clearToDoButton.addClass("row-" + i);
+//   newDiv.append(clearToDoButton);
+//   clearToDoButton.append($("<i class='fa fa-trash fa-lg'></i>"));
+//   rowElement.append(newDiv);
+// }
+
+////////////FUNCTION TO DISPLAY SPINNER WHILE LOADING
+function displayLoadingSpinner() {
+  var spinnerEl = $("<div class='text-center mt-5'><i class='fa fa-spinner fa-spin ' style='font-size:36px'></i> </div>");
+
+  $("#main-body").append(spinnerEl);
 }
